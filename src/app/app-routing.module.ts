0e0cc26e3e4c3820bb9f1feb5818150679a2d684
jacky <knight.jacky@gmail.com>
2018-12-17 20:09:19 -0500

    modify code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HelpLinkComponent } from './page-objects/first-page/help-link/help-link.component';
-import { SecondPageComponent } from './page-objects/second-page/second-page/second-page.component';
+import { SecondPageComponent } from './page-objects/second-page/second-page.component';
 
 const routes: Routes = [
   {path: '', component: HelpLinkComponent},
